c0fb00cd4491e4d34950aad627852bd62f8b34d9
vamsi0122 <suryavamsi766@gmail.com>
2025-01-05 21:05:27 +0530

    New

--- Create_ec2.sh
+++ Create_ec2.sh
@@ -3,24 +3,29 @@
 #NAMES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "dispatch" "web")
 
 #####var######
-NAME=$@
-ImageId=ami-0b4f379183e5706b9
-SecurityGroupId=sg-0d41613ba45142319
-DomainName=bomma.store
-InstanceType=""
-HostedZoneId=Z029188032MG5FWK5IWMU
 
-########
+
+NAMES=$@
+INSTANCE_TYPE=""
+IMAGE_ID=ami-03265a0778a880afb
+SECURITY_GROUP_ID=sg-0b34d8689bd628e3f
+DOMAIN_NAME=joindevops.online
+HOSTED_ZONE_ID=Z0308214GYCUYHGJHT8R
+
+# if mysql or mongodb instance_type should be t3.medium , for all others it is t2.micro
+
 for i in $@
-do
-    if [$i == mysql || $i == mongodb]
+do  
+    if [[ $i == "mongodb" || $i == "mysql" ]]
     then
-      InstanceType="t3.medium"
+        INSTANCE_TYPE="t3.medium"
     else
-      InstanceType="t2.micro"
+        INSTANCE_TYPE="t2.micro"
     fi
     echo "creating $i instance"
-    Ip_Address=$(aws run-instances --image-id $ImageId --instance-type $InstanceType --security-group-ids $SecurityGroupId --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" | jq -r '.Instances[0].PrivateIpAddress')
+    IP_ADDRESS=$(aws ec2 run-instances --image-id $IMAGE_ID  --instance-type $INSTANCE_TYPE --security-group-ids $SECURITY_GROUP_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" | jq -r '.Instances[0].PrivateIpAddress')
+    echo "created $i instance: $IP_ADDRESS"
+
     aws route53 change-resource-record-sets --hosted-zone-id $HOSTED_ZONE_ID --change-batch '
     {
             "Changes": [{
@@ -33,5 +38,4 @@ do
                         }}]
     }
     '
-
 done
