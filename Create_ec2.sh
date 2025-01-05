2276aec83253d4635b3ceec1154691ad7a9d545b
vamsi0122 <suryavamsi766@gmail.com>
2025-01-06 00:04:12 +0530

    New

--- Create_ec2.sh
+++ Create_ec2.sh
@@ -22,7 +22,6 @@ do
     echo "creating $i instance"
     IP_ADDRESS=$(aws ec2 run-instances --image-id $IMAGE_ID  --instance-type $INSTANCE_TYPE --security-group-ids $SECURITY_GROUP_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" | jq -r '.Instances[0].PrivateIpAddress')
     echo "created $i instance: $IP_ADDRESS"
-
     aws route53 change-resource-record-sets --hosted-zone-id $HOSTED_ZONE_ID --change-batch '
     {
             "Changes": [{
